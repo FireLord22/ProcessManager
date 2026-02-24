fd68621f72e459991aa99c46e5b21dd496f36190
FireLord22 <104571252+FireLord22@users.noreply.github.com>
2026-02-24 16:56:42 +0300

    Add Service

---
+++ ProcessManager/Services/AffinityHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProcessManager.Services
+{
+    public static class AffinityHelper
+    {
+        public static bool IsCoreEnabled(IntPtr mask, int coreIndex)
+        {
+            long value = mask.ToInt64();
+            return (value & (1L << coreIndex)) != 0;
+        }
+
+        public static IntPtr BuildMask(bool[] cores)
+        {
+            long mask = 0;
+
+            for (int i = 0; i < cores.Length; i++)
+                if (cores[i]) mask |= (1L << i);
+
+            return new IntPtr(mask);
+        }
+    }
+}
---
+++ ProcessManager/Services/ProcessService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using ProcessManager.Models;
+
+namespace ProcessManager.Services
+{
+    public static class ProcessService
+    {
+        public static List<ProcessTreeNode> GetProcesses()
+        {
+            var list = new List<ProcessTreeNode>();
+
+            foreach (var p in Process.GetProcesses())
+            {
+                try
+                {
+                    list.Add(new ProcessTreeNode
+                    {
+                        Id = p.Id,
+                        Name = p.ProcessName,
+                        Memory = p.WorkingSet64 / 1024 / 1024
+                    });
+                }
+                catch { }
+            }
+
+            return list;
+        }
+
+        public static void KillProcess(int id)
+        {
+            try
+            {
+                Process.GetProcessById(id).Kill();
+            }
+            catch { }
+        }
+
+        public static void SetPriority(int id, ProcessPriorityClass priority)
+        {
+            try
+            {
+                Process.GetProcessById(id).PriorityClass = priority;
+            }
+            catch { }
+        }
+    }
+}
